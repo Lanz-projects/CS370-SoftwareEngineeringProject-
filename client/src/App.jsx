8d066833cf8eab46a2788873cc2a19258e3aff8a
Matthew Wolz <wolzie99@gmail.com>
2025-02-21 00:35:49 -0600

    initial server setup and push

---
+++ client/src/App.jsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState} from 'react'
+
+
+
+
+function App() {
+  const [ backendData, setBackendData] = useState([{}])
+ 
+  useEffect(() =>{
+    fetch("/api").then(
+      response => response.json()
+    ).then(
+      data => {
+        setBackendData(data)
+      }
+    )
+  }, [])
+
+
+  return (
+    <>
+      <header>
+        <h1>Hello World</h1>
+        {(typeof backendData.users === 'undefined') ? (
+          <p>Loading...</p>
+        ) : (
+          backendData.users.map((user, i) => (
+            <p key={i}>{user}</p>
+          ))
+        )}
+       
+       
+      </header>
+    </>
+
+
+  );
+}
+
+export default App
