af1e1988b29823a5da493e46c34eb980e4d2d781
Matthew Wolz <wolzie99@gmail.com>
2025-03-12 15:08:17 -0500

    designing ride listings page

--- client/src/pages/RideListing.jsx
+++ client/src/pages/RideListing.jsx
@@ -1,9 +1,10 @@
 import React, { useCallback, useRef } from "react";
 import { GoogleMap, useJsApiLoader, Marker } from "@react-google-maps/api";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const containerStyle = {
-  width: "100vw",
-  height: "80vh",
+  width: "100%",
+  height: "100%",
 };
 
 const center = {
@@ -28,19 +29,39 @@ const Map = () => {
   if (!isLoaded) return <div>Loading map...</div>;
 
   return (
-    <div className="map-container">
-      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={12} onLoad={onLoad}>
-        <Marker position={center} />
-      </GoogleMap>
-    </div>
+    <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={12} onLoad={onLoad}>
+      <Marker position={center} />
+    </GoogleMap>
   );
 };
 
 const RideListing = () => {
   return (
-    <div>
-      <h1>This is the RideListing page</h1>
-      <Map />
+    <div className="container-fluid d-flex">
+      {/* Map Section (70% - subject to change) */}
+      <div className="col-8 p-0">
+        <Map />
+      </div>
+
+      {/* Ride Listings Section (30% - subject to change) */}
+      <div className="col-4 bg-dark text-white p-4 rounded-3">
+        <h3 className="mb-4">Ride Listings</h3>
+        <ul className="list-unstyled">
+          <li className="mb-3">
+            <div className="border p-3 rounded-3">
+              <h5>Ride to X Destination</h5>
+              <p>Looking for a ride to X. Let me know if you're heading that way!</p>
+            </div>
+          </li>
+          <li className="mb-3">
+            <div className="border p-3 rounded-3">
+              <h5>Going to Y Place</h5>
+              <p>I'm planning to go to Y place. Anyone interested in joining?</p>
+            </div>
+          </li>
+          {/* Add more listings here (likely pull from Firebase/MongoDB) */}
+        </ul>
+      </div>
     </div>
   );
 };
