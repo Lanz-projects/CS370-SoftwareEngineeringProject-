4ad9d064b2f3904c9b9e6ff02fb7a8c91cd8e3f8
Lanz Dungo <lanz_dungo@yahoo.com>
2025-04-15 22:22:34 -0500

    Changed the redirect to complete your setup, so that they are forced to do it.

--- client/src/components/RedirectUserInfoPopup.jsx
+++ client/src/components/RedirectUserInfoPopup.jsx
@@ -9,16 +9,20 @@ function RedirectUserInfoPopup ({ show, onConfirm, onSkip }) {
       <Modal.Body>
         <p>
           To access ride-share features, you need to complete the next step.  
-          If you <em>decide to do this later, you won't be able to use any ride-share features</em>.
+          You will be redirected to complete your profile. Afterwords, you will be 
+          able to use most features. If you decide to offer a post, <em>please register your
+          vehicle in the profile page.</em>
         </p>
       </Modal.Body>
       <Modal.Footer>
         <Button variant="primary" onClick={onConfirm}>
           Go to Next Page
         </Button>
+        {/*
         <Button variant="secondary" onClick={onSkip}>
           Do It Later
         </Button>
+        */} 
       </Modal.Footer>
     </Modal>
   );
