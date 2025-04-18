9a47bf523cd076daa52ea8ecfebc2cd1e348730a
Johake0 <jihoke32@gmail.com>
2025-04-18 13:52:26 -0500

    Revert "Merge branch 'JakeTimer'"
    This reverts commit fd4c24065030b0e17b808982cca02d610df4aa73, reversing
    changes made to a290fbaf7e338352e1d0516f38bcc3c57d2eec92.

--- server/models/User-schema.js
+++ server/models/User-schema.js
@@ -75,12 +75,4 @@ const UserSchema = new mongoose.Schema(
   { collection: "Users" }
 );
 
-<<<<<<< HEAD
 module.exports = mongoose.model("User", UserSchema);
-=======
-
-module.exports = mongoose.model('User', UserSchema);
-UserSchema.index({ createdAt: 1 }, { expireAfterSeconds: 60 }); // ⏱️ 1 minute for testing
-
-
->>>>>>> JakeTimer
